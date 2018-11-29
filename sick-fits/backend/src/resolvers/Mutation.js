cc5a4d9823524b33da3ac1df645e431798c0cd80
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-11-29 12:33:51 -0800

    working on deleteItem #20

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,15 +1,40 @@
-const mutations = {
+const Mutations = {
   async createItem(parent, args, ctx, info) {
     // Check if they are logged in
 
     const item = await ctx.db.mutation.createItem({
       data: {
         ...args
-      }
-    }, info)
+      },
+    }, info);
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. Check if they own that item, or have permission
+
+    // 3. Delete
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
-module.exports = mutations;
+module.exports = Mutations;
--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
 
-// const CREATE_ITEM_MUTATION = gql`
-//   mutation CREATE_ITEM_MUTATION(
-//     title: String!
-//     description: String!
-//     price: Int!
-//     image: String
-//     largeImage: String
-//   ) {
-//     createItem(
-//       title: $title
-//       description: $description
-//       price: $price
-//       image: $image
-//       largeImage: $largeImage
-//     ) {
-//       id
-//     }
-//   }
-// `;
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
 
 class CreateItem extends Component {
   state = {
@@ -37,15 +39,56 @@ class CreateItem extends Component {
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: val });
   };
+  uploadFile = async e => {
+    console.log('uploading file...');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dn3baky3u/image/upload', {
+      method: 'POST',
+      body: data
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
   render() {
-    return 
+    return (
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, {loading, error }) => (
     <Form 
-      onSubmit={e => {
+      onSubmit={ async e => {
         e.preventDefault();
-        console.log(this.state);
+        // call the mutation:
+        const res = await createItem();
+        // Change users to the single item page
+        console.log(res);
+        Router.push({
+          pathname: '/item',
+          query: { id: res.data.createItem.id }
+        });
     }}>
-     <fieldset>
-      <label htmlFor="title">
+     <Error error={error} />
+     <fieldset disabled={loading} aria-busy={loading}>
+     <label htmlFor="title">
+      Image
+      <input 
+        type="file"
+        id="file"
+        name="file"
+        placeholder="Upload an image"
+        required
+        onChange={this.uploadFile}
+      />
+      {this.state.image && <img src= {this.state.image} width="200" alt="Upload Preview" />}
+      </label> 
+     
+     <label htmlFor="title">
       Title
       <input 
         type="text"
@@ -84,7 +127,10 @@ class CreateItem extends Component {
       </label>
       <button type="submit">Submit</button>
      </fieldset>
-    </Form>    
+    </Form>
+    )}
+  </Mutation>
+  );   
   }
 }
 
